6c091c707b09c7d9f4c28fb5b8dafc75387208c6
Margarita555 <a_rita@ukr.net>
2021-09-03 21:19:28 +0300

    +

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,4 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+// 05 Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
 const input = document.querySelector('#name-input');
 const span = document.querySelector('#name-output');
